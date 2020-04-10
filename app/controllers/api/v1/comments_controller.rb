6226667634cc3d71a5891e39ee617cb2071bebbd
sekoudosso82 <sekou.dosso82@gmail.com>
2020-04-10 14:53:09 -0400

    comment code

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index
+        comments = Post.all  
+
+        render(json: comments, except: [:updated_at, :created_at])
+    end
+
+    def create
+        comment = comment.create(comment_params)
+    
+        render json: comment
+    end
+      
+    def update
+        comment = Comment.find(params[:id])
+    
+        comment.update(comment_params)
+    
+        render json: comment, except: [:updated_at, :created_at]
+    end
+    
+    def destroy
+        comment = Comment.find(params[:id])
+    
+        comment.destroy
+    
+        render json: comment
+    end
+    
+      private
+    
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+    
 end
